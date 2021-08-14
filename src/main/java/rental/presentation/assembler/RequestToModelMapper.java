3e37ebd642a208db828e3ac99cab0b91400338d5
xiyu.liang <1595345149@qq.com>
2021-08-15 00:40:48 +0800

    [liang xi yu] add: add RequestToModelMapper file

---
+++ src/main/java/rental/presentation/assembler/RequestToModelMapper.java
@@ -0,0 +1,31 @@
+package rental.presentation.assembler;
+
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.Mapper;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
+import rental.domain.model.House;
+import rental.presentation.dto.request.HouseRequest;
+import rental.utils.DateUtils;
+
+import java.time.LocalDateTime;
+
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface RequestToModelMapper {
+
+    RequestToModelMapper INSTANCE = Mappers.getMapper(RequestToModelMapper.class);
+
+    @Mapping(target = "establishedTime", source = "houseRequest.establishedTime", qualifiedByName = "toTimestamp")
+    @Mapping(target = "createdTime", source = "houseRequest.createdTime", qualifiedByName = "toTimestamp")
+    @Mapping(target = "updatedTime", source = "houseRequest.createdTime", qualifiedByName = "toTimestamp")
+    House mapToPromotionProposalModel(HouseRequest houseRequest);
+
+    @Named("toTimestamp")
+    default long toTimestamp(LocalDateTime localDateTime) {
+        return DateUtils.toTimestamp(localDateTime);
+    }
+}
