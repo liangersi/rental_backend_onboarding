1b358d02f2fa7257e74d5a459e526350012ab648
xiyu.liang <1595345149@qq.com>
2021-08-15 00:30:11 +0800

    [liang xi yu] add: add NotFoundExceptionHandler method

--- src/main/java/rental/presentation/exception/GlobalExceptionHandler.java
+++ src/main/java/rental/presentation/exception/GlobalExceptionHandler.java
@@ -1,9 +1,20 @@
 package rental.presentation.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
+    public ErrorMessage handleNotFoundException(NotFoundException ex) {
+        return new ErrorMessage("Not Found Exception", ex.getMessage());
+    }
 }
