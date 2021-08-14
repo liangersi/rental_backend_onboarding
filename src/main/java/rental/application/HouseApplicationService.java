b03ece7c9134bbd28a40ba4a60e440455669a44c
xiyu.liang <1595345149@qq.com>
2021-08-15 00:40:18 +0800

    [liang xi yu] add: add saveHouseInfo method

--- src/main/java/rental/application/HouseApplicationService.java
+++ src/main/java/rental/application/HouseApplicationService.java
@@ -6,6 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import rental.domain.model.House;
 import rental.domain.repository.HouseRepository;
+import rental.infrastructure.dataentity.HouseEntity;
+import rental.presentation.assembler.ModelToResponseMapper;
+import rental.presentation.assembler.RequestToEntityMapper;
+import rental.presentation.dto.request.HouseRequest;
+import rental.presentation.dto.response.house.HouseResponse;
 import rental.presentation.exception.NotFoundException;
 
 import java.util.Optional;
@@ -24,5 +29,11 @@ public class HouseApplicationService {
         Optional<House> houseOptional = houseRepository.queryOneHouseInfo(houseId);
         houseOptional.orElseThrow(() -> new NotFoundException("not found exception"));
         return houseOptional.get();
+    }
+
+    public HouseResponse saveHouseInfo(HouseRequest houseRequest) {
+        HouseEntity houseEntity = RequestToEntityMapper.INSTANCE.mapToPromotionProposalModel(houseRequest);
+        House houseSaved = houseRepository.saveHouseInfo(houseEntity);
+        return ModelToResponseMapper.INSTANCE.mapToPromotionProposalResponse(houseSaved);
     }
 }
