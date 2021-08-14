64a29ba26cb6e3c727f92c8bd38096b925d48cdf
xiyu.liang <1595345149@qq.com>
2021-08-15 00:39:34 +0800

    [liang xi yu] add: add saveHouseInfo method

--- src/main/java/rental/domain/repository/HouseRepository.java
+++ src/main/java/rental/domain/repository/HouseRepository.java
@@ -3,6 +3,7 @@ package rental.domain.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import rental.domain.model.House;
+import rental.infrastructure.dataentity.HouseEntity;
 
 import java.util.Optional;
 
@@ -10,4 +11,6 @@ public interface HouseRepository {
     Page<House> queryAllHouses(Pageable pageable);
 
     Optional<House> queryOneHouseInfo(Long houseId);
+
+    House saveHouseInfo(HouseEntity houseEntity);
 }
