76a0e36d5dfbbb84c91312359bb5ec4c7da2bfa7
xiyu.liang <1595345149@qq.com>
2021-08-15 00:25:50 +0800

    [liang xi yu] add: add ErrorMessage file

---
+++ src/main/java/rental/presentation/exception/ErrorMessage.java
@@ -0,0 +1,11 @@
+package rental.presentation.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessage {
+
+    private String message;
+}
