70e6d84902bbe5828fe32a2b15c5fa3d5b9901fd
xiyu.liang <1595345149@qq.com>
2021-08-15 00:26:01 +0800

    [liang xi yu] add: add NotFoundException file

---
+++ src/main/java/rental/presentation/exception/NotFoundException.java
@@ -0,0 +1,11 @@
+package rental.presentation.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class NotFoundException extends IllegalArgumentException{
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
