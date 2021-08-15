4723db37164af448cb62745e41c2e6da1b7d1aa7
xiyu.liang <1595345149@qq.com>
2021-08-15 20:19:33 +0800

    [liang xi yu] add: add 3rdClient save method judgement

--- src/main/java/rental/application/HouseApplicationService.java
+++ src/main/java/rental/application/HouseApplicationService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import rental.client.FakeClient;
 import rental.domain.model.House;
 import rental.domain.repository.HouseRepository;
 import rental.infrastructure.dataentity.HouseEntity;
@@ -11,6 +12,7 @@ import rental.presentation.assembler.ModelToResponseMapper;
 import rental.presentation.assembler.RequestToEntityMapper;
 import rental.presentation.dto.request.HouseRequest;
 import rental.presentation.dto.response.house.HouseResponse;
+import rental.presentation.exception.AddThirdClientException;
 import rental.presentation.exception.NotFoundException;
 
 import java.util.Optional;
@@ -20,6 +22,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class HouseApplicationService {
     private final HouseRepository houseRepository;
+    private final FakeClient fakeClient;
 
     public Page<House> queryAllHouses(Pageable pageable) {
         return houseRepository.queryAllHouses(pageable);
@@ -31,9 +34,14 @@ public class HouseApplicationService {
         return houseOptional.get();
     }
 
-    public HouseResponse saveHouseInfo(HouseRequest houseRequest) {
+    public HouseResponse saveHouseInfo(HouseRequest houseRequest) throws AddThirdClientException {
         HouseEntity houseEntity = RequestToEntityMapper.INSTANCE.mapToPromotionProposalModel(houseRequest);
         House houseSaved = houseRepository.saveHouseInfo(houseEntity);
+
+        if (!fakeClient.saveOneHouseInfo(houseSaved)) {
+            houseRepository.deleteHouse(houseEntity);
+            throw new AddThirdClientException("fail update info to 3rd client");
+        }
         return ModelToResponseMapper.INSTANCE.mapToPromotionProposalResponse(houseSaved);
     }
 }
