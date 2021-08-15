2e91bfd1e7e18350c01d1dc5c878dc37ceb6841a
xiyu.liang <1595345149@qq.com>
2021-08-15 14:01:47 +0800

    [liang xi yu] add: add Builder annotation

--- src/main/java/rental/presentation/dto/response/house/HouseResponse.java
+++ src/main/java/rental/presentation/dto/response/house/HouseResponse.java
@@ -1,6 +1,7 @@
 package rental.presentation.dto.response.house;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import rental.domain.model.enums.HouseStatus;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
 public class HouseResponse {
     private Long id;
 
