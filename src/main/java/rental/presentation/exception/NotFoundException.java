c2f14594a98d48bfa0be6e2af7c293a87a64f035
xiyu.liang <1595345149@qq.com>
2021-08-15 14:10:48 +0800

    [liang xi yu] fix: format line: 7

--- src/main/java/rental/presentation/exception/NotFoundException.java
+++ src/main/java/rental/presentation/exception/NotFoundException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class NotFoundException extends IllegalArgumentException{
+public class NotFoundException extends IllegalArgumentException {
     public NotFoundException(String message) {
         super(message);
     }
