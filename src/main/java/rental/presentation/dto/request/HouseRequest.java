b8d628bf3d83895ab3457f86dd714f38fa2d85e6
xiyu.liang <1595345149@qq.com>
2021-08-15 14:01:29 +0800

    [liang xi yu] add: add arguments annotation

--- src/main/java/rental/presentation/dto/request/HouseRequest.java
+++ src/main/java/rental/presentation/dto/request/HouseRequest.java
@@ -1,25 +1,31 @@
 package rental.presentation.dto.request;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import rental.domain.model.enums.HouseStatus;
 
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
 public class HouseRequest {
     private Long id;
 
     private String name;
 
+    @NotNull
     private String location;
 
+    @NotNull
     private BigDecimal price;
 
+    @NotNull
     private LocalDateTime establishedTime;
 
     private HouseStatus status;
