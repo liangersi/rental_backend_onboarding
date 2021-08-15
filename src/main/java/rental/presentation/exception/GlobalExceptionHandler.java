cea8fd1c92f77788857d93fc21623eff64fdf224
xiyu.liang <1595345149@qq.com>
2021-08-15 14:04:23 +0800

    [liang xi yu] add: add handleMethodArgumentNotValidException method

--- src/main/java/rental/presentation/exception/GlobalExceptionHandler.java
+++ src/main/java/rental/presentation/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package rental.presentation.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -16,5 +17,12 @@ public class GlobalExceptionHandler {
     @ResponseBody
     public ErrorMessage handleNotFoundException(NotFoundException ex) {
         return new ErrorMessage("Not Found Exception", ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ErrorMessage handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        return new ErrorMessage("Lack Argument Exception", ex.getMessage());
     }
 }
