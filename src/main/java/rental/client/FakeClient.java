02e6a6919f4cf3bc9a44c7721b85214abbf3e700
xiyu.liang <1595345149@qq.com>
2021-08-15 20:18:21 +0800

    [liang xi yu] add: add FakeClient class

---
+++ src/main/java/rental/client/FakeClient.java
@@ -0,0 +1,13 @@
+package rental.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import rental.domain.model.House;
+
+@FeignClient("fakeApi-service")
+public interface FakeClient {
+
+    @PostMapping("houses/house")
+    Boolean saveOneHouseInfo(@RequestBody House house);
+}
